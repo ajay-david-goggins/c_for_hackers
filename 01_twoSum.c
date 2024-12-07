aad4c7a5a6697220131b5906944b24110ba0bd31
Ajay <ajaykarunakaran987@gmail.com>
2024-12-07 23:05:35 +0530

    updated two sum problem using O(n^2)

--- 01_twoSum.c
+++ 01_twoSum.c
@@ -1,15 +1,17 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+
    *returnSize = 2; 
    int* returnArray = (int*) malloc(*returnSize * sizeof(int)); 
    
    for (int i = 0; i < numsSize - 1; i++) {
-	   for (int j = i + 1; i < numsSize; j++) {
+	   for (int j = i + 1; j < numsSize; j++) {
 		   if (target == nums[i] + nums[j]) {
 			   returnArray[0] = i;
 			   returnArray[1] = j;
@@ -23,11 +25,24 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
 }
 
 int main() {
-	int arr[] = {7, 2, 8, 6};
-	int target = 9;
+	int n;
+	printf("Enter the number : ");
+	scanf("%d", &n);
+	int arr[n];
+	int returnsize;
+	int target;
 	int size = sizeof(arr) / sizeof(arr[0]);
-	int returnsize;	
+	
+	for (int i = 0; i < n; i++) {
+		printf("Enter the value of a[%d]  : ", i );
+		scanf("%d",&arr[i]);
+	}
+
+	printf("Enter the target number. it must be sum of two value in the array : ");
+	scanf("%d", &target);
+
 	int *ans = twoSum(arr, size, target, &returnsize); 
+
 	if (ans != NULL) {
 		printf("%d , %d ", ans[0], ans[1]);
 		free(ans);
